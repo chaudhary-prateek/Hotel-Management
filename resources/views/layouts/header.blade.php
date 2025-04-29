2be35ab558cf36570d42b958b1daa67b3fd34779
Prateek Chaudhary <prateekchaudhary7805@gmail.com>
2025-04-29 13:50:40 +0530

    main update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
         <div class="col-lg-3 bg-dark d-none d-lg-block">
             <a href="{{ route('home') }}"
                class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
-                <h1 class="m-0 text-primary text-uppercase">Prateek Hotel</h1>
+                <h1 class="m-0 text-primary text-uppercase">Pratik Hotel</h1>
             </a>
         </div>
         <div class="col-lg-9">
